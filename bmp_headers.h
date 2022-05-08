377a42d950dce18aa730b9db8cff577224713e0e
Jędrzej Pacanowski <42004734+jotpacanowski@users.noreply.github.com>
2022-05-08 18:21:28 +0200

    Add newer BMP headers

--- bmp_headers.h
+++ bmp_headers.h
@@ -1,6 +1,4 @@
 #pragma once
-// Source: https://www.cs.put.poznan.pl/tzok/public/cawllp-03-bmp.html
-#include <stdio.h>
 #include <stdint.h>
 
 typedef uint16_t WORD;
@@ -8,6 +6,20 @@ typedef uint32_t DWORD;
 typedef int32_t LONG;
 
 #pragma pack(push,1)
+
+// https://docs.microsoft.com/en-us/windows/win32/api/wingdi/ns-wingdi-ciexyz
+// https://docs.microsoft.com/en-us/windows/win32/api/wingdi/ns-wingdi-ciexyztriple
+typedef int32_t FXPT2DOT30;
+typedef struct tagCIEXYZ {
+  FXPT2DOT30 ciexyzX;
+  FXPT2DOT30 ciexyzY;
+  FXPT2DOT30 ciexyzZ;
+} CIEXYZ;
+typedef struct tagCIEXYZTRIPLE {
+  CIEXYZ ciexyzRed;
+  CIEXYZ ciexyzGreen;
+  CIEXYZ ciexyzBlue;
+} CIEXYZTRIPLE;
 
 // https://docs.microsoft.com/en-us/windows/win32/api/wingdi/ns-wingdi-bitmapfileheader
 typedef struct tagBITMAPFILEHEADER {
@@ -19,6 +31,8 @@ typedef struct tagBITMAPFILEHEADER {
 } BITMAPFILEHEADER, *LPBITMAPFILEHEADER, *PBITMAPFILEHEADER;
 
 // https://docs.microsoft.com/pl-pl/previous-versions/dd183376(v=vs.85)
+// https://docs.microsoft.com/pl-pl/windows/win32/api/wingdi/ns-wingdi-bitmapv4header
+//
 typedef struct tagBITMAPINFOHEADER {
   DWORD biSize;
   LONG  biWidth;
@@ -31,6 +45,21 @@ typedef struct tagBITMAPINFOHEADER {
   LONG  biYPelsPerMeter;
   DWORD biClrUsed;
   DWORD biClrImportant;
+  // -- version 4
+  DWORD        bV4RedMask;
+  DWORD        bV4GreenMask;
+  DWORD        bV4BlueMask;
+  DWORD        bV4AlphaMask;
+  DWORD        bV4CSType;
+  CIEXYZTRIPLE bV4Endpoints;
+  DWORD        bV4GammaRed;
+  DWORD        bV4GammaGreen;
+  DWORD        bV4GammaBlue;
+  // -- version 5
+  DWORD        bV5Intent;
+  DWORD        bV5ProfileData;
+  DWORD        bV5ProfileSize;
+  DWORD        bV5Reserved;
 } BITMAPINFOHEADER, *LPBITMAPINFOHEADER, *PBITMAPINFOHEADER;
 
 #pragma pack(pop)
