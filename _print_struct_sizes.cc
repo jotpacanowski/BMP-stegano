a20c3ca5ca16f8d775ecef4fb3c0fcd0f8e72799
Jędrzej Pacanowski <42004734+jotpacanowski@users.noreply.github.com>
2022-05-08 18:57:41 +0200

    Add helper program for struct size printing

---
+++ _print_struct_sizes.cc
@@ -0,0 +1,21 @@
+#define _WINDOWS_LEAN_AND_MEAN 1
+#include <stdio.h>
+#include <windows.h>
+
+#define TS(x,y) printf("sizeof " #x " is %zu = " #y "\n", sizeof(x));
+#define PS(x) printf("sizeof " #x " = %zu\n", sizeof(x));
+int main(){
+	TS(WORD, 2)
+	TS(DWORD, 4)
+	TS(LONG, 4)
+	puts("");
+	TS(FXPT2DOT30, 4)
+	TS(CIEXYZ, 3*4)
+	TS(CIEXYZTRIPLE, 3*3*4)
+	puts("");
+	PS(BITMAPFILEHEADER);
+	PS(BITMAPINFOHEADER);
+	PS(BITMAPV4HEADER);
+	PS(BITMAPV5HEADER);
+	return 0;
+}
